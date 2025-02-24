cb4fcf70c0a7784ec1891ef6946ad2ff18d3f91d
Anghel941 <80909584+Anghel941@users.noreply.github.com>
2025-02-24 12:16:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    // Al cargar la página, ocultamos las cortinas
+    $('.left-curtain').css('width', '0%');
+    $('.right-curtain').css('width', '0%');
+  
+    $('.valentines-day').click(function(){
+      // Animación de desvanecimiento de los elementos del sobre
+      $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
+      $('.envelope').fadeOut(800, function() {
+        // Ocultar elementos dentro de .valentines-day
+        $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+        
+  
+        // Hacer visible la carta con una animación ondulante
+        $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
+        $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
+          var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
+          $(this).css('transform', 'scale(' + scale + ')');
+        }}); // Animación de ondulación
+      });
+    });
+  });
+
+   // Código JavaScript para cambiar el color del corazón cada cierto tiempo
+   const heart = document.querySelector('.heart2');
+
+   function changeColor() {
+     heart.style.backgroundColor = 'red';
+   }
+
+   setInterval(changeColor, 2000); // Cambiar el color cada 2 segundos
+  
